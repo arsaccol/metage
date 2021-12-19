9dfcfd9e879fc9200945745a7cc3d93eee9def96
Alexandre R. Saccol <arsaccol@gmail.com>
2021-12-19 01:17:33 -0300

    We can walk backwards and forwards in single fixed steps

--- client/src/controls.js
+++ client/src/controls.js
@@ -1,4 +1,5 @@
 import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls'
+import * as THREE from 'three'
 
 class Controls
 {
@@ -31,10 +32,23 @@ class Controls
         this.controls.addEventListener('change', (e)  => {
         })
 
-        document.body.addEventListener('keydown', (e) => {
-            console.log('key down!')
-        })
 
+        document.body.addEventListener('keydown', (e) => {
+            if(this.controls.isLocked) {
+                const keyCode = String.fromCharCode(e.keyCode)
+
+                const lookDirection = new THREE.Vector3()
+                this.camera.getWorldDirection(lookDirection)
+
+                if( keyCode === 'W') {
+                    this.camera.position.add(lookDirection)
+                }
+                if( keyCode === 'S') {
+                    this.camera.position.add(lookDirection.negate())
+                    console.log(this.camera.position)
+                }
+            }
+        })
     }
 
 
