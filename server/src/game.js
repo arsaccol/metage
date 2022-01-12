1966a68b0ab6168f0540d7f8f594e2ab5318d872
Alexandre R. Saccol <arsaccol@gmail.com>
2022-01-12 09:24:19 -0300

    Properly use onPlayerSpawnRequest on relevant event, rather than hardcoded callback

--- server/src/game.js
+++ server/src/game.js
@@ -6,8 +6,6 @@ class Game
 {
     constructor()
     {
-        // for now, there's no compensation for the case
-        // where game loop iterations take longer than the tick rate
         this.tickRate = 30 // FPS, but without a frame; tentative value
         this.clock = new THREE.Clock()
         this.scene = new THREE.Scene()
@@ -30,6 +28,11 @@ class Game
     // run and define the game loop I guess
     runLoop()
     {
+        /////////////////////////////////////////////////////////////////////
+            // for now, there's no compensation for the case
+            // where game loop iterations take longer than the tick rate
+        /////////////////////////////////////////////////////////////////////
+
         // actual game loop iteration 
         setInterval( () => {
             console.log(`Delta time: ${1 / this.clock.getDelta()}`)
@@ -45,34 +48,9 @@ class Game
     onConnection = (socket) => 
     {
         console.log(`${socket.id} connected to server at ${new Date().toISOString()}!`)
-        socket.on('disconnect', () => { this.onDisconnect(socket) } )
+        socket.on('disconnect', () => { this.onDisconnect(socket) })
 
-        socket.on('client:spawn-request', () => {
-
-            console.log(`Spawning new player with ID: ${socket.id}!`)
-
-            const newPlayerObject3D = new THREE.Object3D()
-            newPlayerObject3D.position.set(Math.random() * 10 - 5, 1, Math.random() * 10 - 5)
-            newPlayerObject3D.lookAt(new THREE.Vector3())
-            newPlayerObject3D.rotateY(Math.PI)
-
-            this.players[socket.id] = newPlayerObject3D
-            const {position, quaternion} = {...newPlayerObject3D}
-
-            const stateOfExistingPlayers = Object.keys(this.players).filter(id => id !== socket.id).map( id => {
-                const { position, quaternion } = this.players[id]
-                return {
-                    id: id,
-                    position: position,
-                    quaternion: quaternion
-                }
-            } )
-
-            socket.emit('server:spawn-response', {spawnedPlayer: {position: position, quaternion: quaternion}, existingPlayers: stateOfExistingPlayers})
-
-            console.log(`Players connected: [${Object.keys(this.players)}]`)
-            socket.broadcast.emit('other-clients:spawn-broadcast', {id: socket.id, position: position, quaternion: quaternion})
-        })
+        socket.on('client:spawn-request', () => { this.onClienSpawnRequest(socket) })
     }
 
     onDisconnect = (socket) =>
@@ -82,8 +60,10 @@ class Game
         delete this.players[socket.id]
     }
 
-    onClienSpawnRequest = (playerID) =>
+    onClienSpawnRequest = (socket) =>
     {
+        console.log(`Spawning new player with ID: ${socket.id}!`)
+
         const newPlayerObject3D = new THREE.Object3D()
         newPlayerObject3D.position.set(Math.random() * 10 - 5, 1, Math.random() * 10 - 5)
         newPlayerObject3D.lookAt(new THREE.Vector3())
@@ -92,8 +72,19 @@ class Game
         this.players[socket.id] = newPlayerObject3D
         const {position, quaternion} = {...newPlayerObject3D}
 
-        socket.emit('server:spawn-response', {position: position, quaternion: quaternion})
+        const stateOfExistingPlayers = Object.keys(this.players).filter(id => id !== socket.id).map( id => {
+            const { position, quaternion } = this.players[id]
+            return {
+                id: id,
+                position: position,
+                quaternion: quaternion
+            }
+        } )
+
+        socket.emit('server:spawn-response', {spawnedPlayer: {position: position, quaternion: quaternion}, existingPlayers: stateOfExistingPlayers})
+
         console.log(`Players connected: [${Object.keys(this.players)}]`)
+        socket.broadcast.emit('other-clients:spawn-broadcast', {id: socket.id, position: position, quaternion: quaternion})
     }
 
 
